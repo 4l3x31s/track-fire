914c3c0ca510852d2b2d6c7c0e1313b75919e571
Alexeis Vladimir Carrillo Pinaya <acarrillo@mail.rua.net>
2019-01-30 18:31:51 -0400

    tracking

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,18 +9,42 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireDatabaseModule, AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { Device } from '@ionic-native/device/ngx';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+
+import * as firebase from 'Firebase';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDR8UMcHDWYnf7TVjE0Z6r5aKE3viAwnyk',
+  authDomain: 'geotracker-79f2e.firebaseapp.com',
+  databaseURL: 'https://geotracker-79f2e.firebaseio.com',
+  projectId: 'geotracker-79f2e',
+  storageBucket: 'geotracker-79f2e.appspot.com',
+  messagingSenderId: '729611587224'
+};
+firebase.initializeApp(firebaseConfig);
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
     AppRoutingModule
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    AngularFireDatabase,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    Geolocation,
+    Device
   ],
   bootstrap: [AppComponent]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import * as firebase from 'Firebase';
+import {Platform} from '@ionic/angular';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import { Device } from '@ionic-native/device/ngx';
+
+declare var google: any;
 
 @Component({
   selector: 'app-home',
@@ -6,5 +12,102 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
+  markers = [];
+  ref = firebase.database().ref('geolocations/');
+  constructor(public platform: Platform,
+              private geolocation: Geolocation,
+              private device: Device) {
+    platform.ready().then(() => {
+      this.initMap();
+    });
+    this.ref.on('value', resp => {
+      this.deleteMarkers();
+      snapshotToArray(resp).forEach(data => {
+        if (data.uuid !== this.device.uuid) {
+          const image = 'assets/imgs/green-bike.png';
+          const updatelocation = new google.maps.LatLng(data.latitude, data.longitude);
+          this.addMarker(updatelocation, image);
+          this.setMapOnAll(this.map);
+        } else {
+          const image = 'assets/imgs/blue-bike.png';
+          const updatelocation = new google.maps.LatLng(data.latitude, data.longitude);
+          this.addMarker(updatelocation, image);
+          this.setMapOnAll(this.map);
+        }
+      });
+    });
+  }
+  initMap() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      const mylocation = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+      this.map = new google.maps.Map(this.mapElement.nativeElement, {
+        zoom: 15,
+        center: mylocation
+      });
+    });
+    const watch = this.geolocation.watchPosition();
+    watch.subscribe((data) => {
+      this.deleteMarkers();
+      this.updateGeolocation(this.device.uuid, data.coords.latitude, data.coords.longitude);
+      const updatelocation = new google.maps.LatLng(data.coords.latitude, data.coords.longitude);
+      const image = 'assets/imgs/blue-bike.png';
+      this.addMarker(updatelocation, image);
+      this.setMapOnAll(this.map);
+    });
+  }
 
+  addMarker(location, image) {
+    const marker = new google.maps.Marker({
+      position: location,
+      map: this.map,
+      icon: image
+    });
+    this.markers.push(marker);
+  }
+
+  setMapOnAll(map) {
+    for (let i = 0; i < this.markers.length; i++) {
+      this.markers[i].setMap(map);
+    }
+  }
+
+  clearMarkers() {
+    this.setMapOnAll(null);
+  }
+
+  deleteMarkers() {
+    this.clearMarkers();
+    this.markers = [];
+  }
+
+  updateGeolocation(uuid, lat, lng) {
+    if(localStorage.getItem('mykey')) {
+      firebase.database().ref('geolocations/' + localStorage.getItem('mykey')).set({
+        uuid: uuid,
+        latitude: lat,
+        longitude : lng
+      });
+    } else {
+      const newData = this.ref.push();
+      newData.set({
+        uuid: uuid,
+        latitude: lat,
+        longitude: lng
+      });
+      localStorage.setItem('mykey', newData.key);
+    }
+  }
 }
+export const snapshotToArray = snapshot => {
+  const returnArr = [];
+
+  snapshot.forEach(childSnapshot => {
+    const item = childSnapshot.val();
+    item.key = childSnapshot.key;
+    returnArr.push(item);
+  });
+
+  return returnArr;
+};
